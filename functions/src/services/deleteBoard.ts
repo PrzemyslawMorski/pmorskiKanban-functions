1c487ba639d8ce85818141b8c8827d5c9bac2d91
pmorski <morski.przemek@gmail.com>
2018-10-30 20:40:25 +0100

    added catch clauses to all promises

--- functions/src/services/deleteBoard.ts
+++ functions/src/services/deleteBoard.ts
@@ -30,22 +30,27 @@ export const deleteSubcollectionsService = (snap: FirebaseFirestore.DocumentSnap
     snap.ref.getCollections()
         .then(collections => {
             collections.forEach(collection => {
-                collection.get().then(docSnaps => {
-                    docSnaps.forEach((docSnap => {
-                        docSnap.ref.delete()
-                            .then(() => {
-                                deleteSubcollectionsService(docSnap);
-                            })
-                            .catch((err) => {
-                                console.log(err);
-                                console.log('err while deleting ' + docSnap.id);
-                            });
-                    }))
-                })
+                collection.get()
+                    .then(docSnaps => {
+                        docSnaps.forEach((docSnap => {
+                            docSnap.ref.delete()
+                                .then(() => {
+                                    deleteSubcollectionsService(docSnap);
+                                })
+                                .catch((err) => {
+                                    console.log(err);
+                                    console.log('err while deleting docSnap ' + docSnap.id);
+                                });
+                        }))
+                    })
+                    .catch((err) => {
+                        console.log(err);
+                        console.log('err while getting docsnaps from collection ' + collection.id);
+                    })
             })
         })
         .catch((err) => {
             console.log(err);
-            console.log('err while getting collections ' + snap.id);
+            console.log('err while getting collections from docsnap ' + snap.id);
         })
 }
